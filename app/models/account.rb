c9d31295e947c286d2ebd2c4c0a4432c1bb47d25
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-11-11 10:30:26 -0200

    Simple balance calculation, validation for transfer

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,12 +1,18 @@
 class Account < ApplicationRecord
   self.inheritance_column = 'record_type'
-  has_many :credits
-  has_many :recurring_credits
-  has_many :debits
-  has_many :recurring_debits
+  has_many :credits, -> { order(:date) }
+  has_many :recurring_credits, -> { order(:name) }
+  has_many :debits, -> { order(:date) }
+  has_many :recurring_debits, -> { order(:date) }
   has_many :transfers, through: :debits
 
   validates :name, presence: true, uniqueness: true
 
   enum type: { generic: 0, bank_account: 1, credit_card: 2 }
+
+  # Simple account balance, not taking into account expired credits
+  # nor this account's possible expiration/closing date.
+  def balance(date = DateTime.now)
+    debits.where('"date" <= ?', date).sum(:value) - credits.where('"date" <= ?', date).sum(:value)
+  end
 end
--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,4 +1,12 @@
 class Transfer < ApplicationRecord
   belongs_to :debit
   belongs_to :credit
+
+  validate :debit_and_credit_value_must_be_the_same
+
+  def debit_and_credit_value_must_be_the_same
+    unless debit.present? && credit.present? && debit.value == credit.value
+      errors.add(:base, "Debit and credit values must be the same")
+    end
+  end
 end
