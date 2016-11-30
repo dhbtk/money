da2cf48e8531682eb4c70deb3c7a0edba58d5cbb
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-11-30 14:28:32 -0200

    Table view for statements

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -1,8 +1,34 @@
 class StatementsController < ApplicationController
   def index
-    @from = params[:from].present? ? params[:from] : DateTime.now.beginning_of_month
-    @to = DateTime.now.to_date
-    @statements = current_user.statements.where('"date" >= ? AND "date" < ?', @from, @to).order(date: :desc, created_at: :desc)
+    @options = {
+        'Semana atual' => 'week',
+        'Mês atual' => 'month',
+        'Últimos três meses' => 'trimester',
+        'Futuros' => 'future'
+    }
+    @selected_option = params[:option].present? ? params[:option] : 'week'
+    @statements = current_user.statements
+    case @selected_option
+      when 'week'
+        @from = DateTime.now.beginning_of_week
+        @to = DateTime.now.to_date
+      when 'month'
+        @from = DateTime.now.beginning_of_month
+        @to = DateTime.now.to_date
+      when 'trimester'
+        @from = (DateTime.now - 2.months).beginning_of_month
+        @to = DateTime.now.to_date
+      when 'future'
+        @from = DateTime.now.to_date + 1.day
+        @to = nil
+      else
+        @from = nil
+        @to = nil
+    end
+    @statements = @statements.where('"date" >= ?', @from.to_date) if @from
+    @statements = @statements.where('"date" <= ?', @to.to_date) if @to
+    @statements = @statements.where(account_id: params[:account_id]) if params[:account_id].present?
+    @statements = @statements.order(date: :desc, created_at: :desc).page(params[:page])
   end
 
   def graph
