7f5e1e39ba9e06984afc1e409e61330de330f263
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-11-30 08:20:03 -0200

    CSS corrections

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -2,14 +2,14 @@ class StatementsController < ApplicationController
   def index
     @from = params[:from].present? ? params[:from] : DateTime.now.beginning_of_month
     @to = DateTime.now.to_date
-    @statements = Statement.all(current_user, @from, @to)
+    @statements = current_user.statements.where('"date" >= ? AND "date" < ?', @from, @to).order(date: :desc, created_at: :desc)
   end
 
   def graph
     @credits = current_user.credits.where('"date" >= ? AND "date" <= ?', DateTime.now.beginning_of_month, DateTime.now.to_date).where.not(id: current_user.transfers.pluck(:credit_id))
     @debits = current_user.debits.where('"date" >= ? AND "date" <= ?', DateTime.now.beginning_of_month, DateTime.now.to_date).where.not(id: current_user.transfers.pluck(:debit_id))
     @from = params[:from].present? ? params[:from] : 7.days.ago.to_date
-    @to = params[:to].present? ? params[:to] : DateTime.now.to_date
-    @statements = Statement.by_date(current_user, @from, @to)
+    @to = DateTime.now.to_date
+    @statements = current_user.statements.where('"date" >= ? AND "date" < ?', @from, @to).order(date: :desc, created_at: :desc).group_by{|x| x.date}
   end
 end
