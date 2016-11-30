3616c2f1a1df3fab3e3dcf12e593e3c9d6d46e5a
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-11-30 14:30:11 -0200

    Unaccent extension

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129160450) do
+ActiveRecord::Schema.define(version: 20161130162853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
