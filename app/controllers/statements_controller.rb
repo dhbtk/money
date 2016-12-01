d1668d5960eaa982cb75fb133d1ce491dbed703f
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-12-01 16:02:12 -0200

    Statement period changed to last 30 days

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -35,8 +35,8 @@ class StatementsController < ApplicationController
   end
 
   def graph
-    @credits = current_user.credits.where('"date" >= ? AND "date" <= ?', DateTime.now.beginning_of_month, DateTime.now.to_date).where.not(id: current_user.transfers.pluck(:credit_id))
-    @debits = current_user.debits.where('"date" >= ? AND "date" <= ?', DateTime.now.beginning_of_month, DateTime.now.to_date).where.not(id: current_user.transfers.pluck(:debit_id))
+    @credits = current_user.credits.where('"date" >= ? AND "date" <= ?', (DateTime.now - 30.days).to_date, DateTime.now.to_date).where.not(id: current_user.transfers.pluck(:credit_id))
+    @debits = current_user.debits.where('"date" >= ? AND "date" <= ?', (DateTime.now - 30.days).to_date, DateTime.now.to_date).where.not(id: current_user.transfers.pluck(:debit_id))
     @from = params[:from].present? ? params[:from] : 7.days.ago.to_date
     @to = DateTime.now.to_date
     @statements = current_user.statements.where('"date" >= ? AND "date" <= ?', @from, @to).order(date: :desc, created_at: :desc).group_by{|x| x.date}
