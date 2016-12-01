d4de12bcbb75dc721fa4f0dc7ecd3a8ae26fcf7b
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-12-01 12:58:56 -0200

    Correction for statements not appearing in graph view

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -37,6 +37,6 @@ class StatementsController < ApplicationController
     @debits = current_user.debits.where('"date" >= ? AND "date" <= ?', DateTime.now.beginning_of_month, DateTime.now.to_date).where.not(id: current_user.transfers.pluck(:debit_id))
     @from = params[:from].present? ? params[:from] : 7.days.ago.to_date
     @to = DateTime.now.to_date
-    @statements = current_user.statements.where('"date" >= ? AND "date" < ?', @from, @to).order(date: :desc, created_at: :desc).group_by{|x| x.date}
+    @statements = current_user.statements.where('"date" >= ? AND "date" <= ?', @from, @to).order(date: :desc, created_at: :desc).group_by{|x| x.date}
   end
 end
