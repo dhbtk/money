feb10e5a54d8a4bc9e07ed1e0e25403eeadd5b54
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-12-02 17:42:23 -0200

    Dashboard fixes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   def show
     @spending = current_user.spending(30)
-    @latest_spending = current_user.credits.where.not(id: current_user.transfers.pluck(:credit_id)).limit(15)
+    @latest_spending = current_user.credits.except(:order).where('"date" <= ?', Date.today).where.not(id: current_user.transfers.pluck(:credit_id)).order(date: :desc, created_at: :desc).limit(15)
+    @tags = current_user.tags
   end
 end
