e829ace117f7c506851662c6641082b5a10667e3
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-12-12 17:27:13 -0200

    Recent spending by tag

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < AuthenticatedController
   def show
     @spending = current_user.spending(30)
     @latest_spending = current_user.credits.except(:order).where('"date" <= ?', Date.today).where.not(id: current_user.transfers.pluck(:credit_id)).order(date: :desc, created_at: :desc).limit(15)
-    @tags = current_user.tags
+    @tags = current_user.tags.with_recent_spending
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -8,4 +8,8 @@ class Tag < ApplicationRecord
   has_many :debits
 
   validates :name, presence: true, uniqueness: { scope: :user_id }
+
+  scope :with_recent_spending, -> do
+    unscope(:order).select("id, name, icon, COALESCE((SELECT sum(value) FROM statements s JOIN accounts a ON s.account_id = a.id WHERE s.type = 'Credit' AND date(date) <= '#{Date.today}' AND date(date) >= '#{15.days.ago.to_date.to_s}' AND tag_id = \"tags\".\"id\"), 0) as credit_total").order('credit_total DESC')
+  end
 end
