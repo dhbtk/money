2867ba698e99def4f4b284a30143f888dccc9d7e
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2016-12-22 15:08:15 -0200

    Fixed not being able to remove the filtering on the statement list

--- app/controllers/statements_controller.rb
+++ app/controllers/statements_controller.rb
@@ -16,7 +16,7 @@ class StatementsController < AuthenticatedController
         'Transferências' => 'transfer'
     }
     @selected_period = params[:period].present? ? params[:period] : (session[:statements_period] || 'week')
-    @type = params[:type].blank? ? (session[:statements_type] || '') : params[:type]
+    @type = !params[:type].nil? ? (session[:statements_type] || '') : params[:type]
 
     @statements = current_user.statements.skip_transfer_debits
     case @selected_period
