4818c41048f171c387b0f610aae602495c2f7d5e
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2017-02-07 11:21:12 -0200

    Report colors

--- app/models/category.rb
+++ app/models/category.rb
@@ -10,4 +10,8 @@ class Category < ApplicationRecord
   scope :with_recent_spending, -> do
     unscope(:order).select("id, name, icon, COALESCE((SELECT sum(value) FROM statements s JOIN accounts a ON s.account_id = a.id WHERE s.type = 'Credit' AND date(date) <= '#{Date.today}' AND date(date) >= '#{15.days.ago.to_date.to_s}' AND category_id = \"categories\".\"id\"), 0) as credit_total").order('credit_total DESC')
   end
+
+  def color
+  	  "##{(self.hash % 0xDDDDDD).to_s(16)}"
+  end
 end
