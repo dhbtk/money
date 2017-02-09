e790b167fb6ee176d86050f6580534db4bc91608
Eduardo Antonio Niehues <eduardo.antonio@eits.com.br>
2017-02-09 15:38:01 -0200

    fixed default sort for bills

--- app/models/billing_account.rb
+++ app/models/billing_account.rb
@@ -1,7 +1,7 @@
 class BillingAccount < ApplicationRecord
   audited
   belongs_to :user
-  has_many :bills, -> { order(expiration: :asc, created_at: :asc) }, dependent: :destroy
+  has_many :bills, -> { order(expiration: :desc, created_at: :asc) }, dependent: :destroy
 
   validates :name, presence: true
 end
